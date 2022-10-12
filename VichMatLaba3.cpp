a7808df79dde2a79a282b7c622f5424f5d3b8da8
Максим Дмитриевич Мальцев <MaltsevMD@stud.rosnou.ru>
2022-10-12 21:12:54 +0300

    4

--- VichMatLaba3.cpp
+++ VichMatLaba3.cpp
@@ -3,8 +3,8 @@
 
 #include <iostream>
 
-double myexp(double x) {
-	double res = 1 + x, fact = 1, xx = x, acc = 10 * pow(0.1, 100), add;
+double myexp(double x, bool flag) {
+	double x1 = fmod(x, floor(x)), res = 1 + x1, fact = 1, xx = x1, acc = 10 * pow(0.1, 10), add;
 	int i = 2;
 
 	do {
@@ -13,17 +13,18 @@ double myexp(double x) {
 		++i;
 	} while (abs(add) > acc);
 
-	return res;
+	if (flag) return res * myexp(floor(x), 0);
+	else return res;
 }
 
-double gaussacc = 10 * pow(0.1, 1000), x = -6.23, gauss, tmp, tmp1;
+double gaussacc = 10 * pow(0.1, 100), x = 0.01, gauss, tmp, tmp1;
 double sqrt2pi = 1 / sqrt(2 * std::atan(1) * 4);
 
 int main()
 {
 	while (1) {
 		tmp1 = x * x / 2 * -1;
-		tmp = myexp(tmp1);
+		tmp = myexp(tmp1, 1);
 		gauss = sqrt2pi * tmp;
 
 		if (gaussacc >= gauss) {
